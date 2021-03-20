d405678477670d058738846c7021f14f058c0d46
SARATOV\Aleksei_Petrov1 <Aleksei_Petrov1@epam.com>
2021-03-20 22:27:24 +0400

    Add DAL project

---
+++ TRPO.Parking/TRPO.Parking.Repositories/Implementations/TestClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TRPO.Parking.Repositories.Interfaces;
+
+namespace TRPO.Parking.Repositories.Implementations
+{
+    public class TestClass : ITestInterface
+    {
+    }
+}
