818b535c764ed2457c5056511fffd2002df2c063
SARATOV\Aleksei_Petrov1 <Aleksei_Petrov1@epam.com>
2021-03-20 23:43:04 +0400

    Add dependencies project

---
+++ TRPO.Parking/TRPO.Parking.Dependencies/DependencyResolver.cs
@@ -0,0 +1,19 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRPO.Parking.Dependencies
+{
+    public class DependencyResolver
+    {
+
+
+        static DependencyResolver()
+        {
+            var builder = new ContainerBuilder();
+        }
+    }
+}
