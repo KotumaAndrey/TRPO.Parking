a337c99d6189e7ebb4b40950ad5b646dca4b29ce
SARATOV\Aleksei_Petrov1 <Aleksei_Petrov1@epam.com>
2021-03-20 22:09:15 +0400

    Add mobile app

---
+++ TRPO.Parking/TRPO.Parking.MobileApp/TRPO.Parking.MobileApp/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TRPO.Parking.MobileApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void OnButtonClicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("АСТАРОЖНА", "Вы включили телефон, а он не настоящий!1!", "Ок", "Не ок");
+        }
+    }
+}
