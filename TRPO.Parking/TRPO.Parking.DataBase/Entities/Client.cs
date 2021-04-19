fdf602e1cd2e77dfd995efbeb5949e9b20db447b
crushwall <crushwall.dev@gmail.com>
2021-04-19 16:37:01 +0400

    Add nullable properties

--- TRPO.Parking/TRPO.Parking.DataBase/Entities/Client.cs
+++ TRPO.Parking/TRPO.Parking.DataBase/Entities/Client.cs
@@ -1,10 +1,10 @@
-﻿using System;
-using TRPO.Parking.DataBase.EntityInterfaces;
+﻿using TRPO.Parking.DataBase.EntityInterfaces;
 using TRPO.Parking.DataBase.EnumEntities;
 using TRPO.Parking.Entities.Primitives;
 
 namespace TRPO.Parking.DataBase.Entities
 {
+#nullable enable
     public class Client : IEntityWithIntId
     {
         public int Id { get; set; }
@@ -12,19 +12,19 @@ namespace TRPO.Parking.DataBase.Entities
         public string Password { get; set; }
 
         public string Name { get; set; }
-        public int Age { get; set; }
+        public int? Age { get; set; }
         
-        public Gender GenderId { get; set; }
-        public virtual GenderEntity Gender { get; set; }
-        
+        public Gender? GenderId { get; set; }
+        public virtual GenderEntity? Gender { get; set; }
+
         public string PhoneNumber { get; set; }
-        public string Email { get; set; }
-        public int Experience { get; set; }
+        public string? Email { get; set; }
+        public int? Experience { get; set; }
 
         public ClientType ClientTypeId { get; set; }
         public virtual ClientTypeEntity ClientType { get; set; }
         
-        public DateTime LatePaymentTimeLeft { get; set; }
+        public int LatePaymentMinutesLeft { get; set; }
         public double LatePaymentPriceMultiplier { get; set; }
     }
 }
--- TRPO.Parking/TRPO.Parking.Entities/Client.cs
+++ TRPO.Parking/TRPO.Parking.Entities/Client.cs
@@ -1,20 +1,20 @@
-﻿using System;
-using TRPO.Parking.Entities.Primitives;
+﻿using TRPO.Parking.Entities.Primitives;
 
 namespace TRPO.Parking.Entities
 {
+#nullable enable
     public class Client 
     {
         public int Id { get; set; }
         public string Password { get; set; }
         public string Name { get; set; }
-        public int Age { get; set; }
-        public virtual Gender Gender { get; set; }
+        public int? Age { get; set; }
+        public virtual Gender? Gender { get; set; }
         public string PhoneNumber { get; set; }
-        public string Email { get; set; }
-        public int Experience { get; set; }
+        public string? Email { get; set; }
+        public int? Experience { get; set; }
         public virtual ClientType ClientType { get; set; }
-        public DateTime LatePaymentTimeLeft { get; set; }
+        public int LatePaymentMinutesLeft { get; set; }
         public double LatePaymentPriceMultiplier { get; set; }
     }
 }
